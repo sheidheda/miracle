b04eaca29f28d1f1e71eb38f24ead34e6c2c31fe
Faithful Ajah <100934910+sheidheda@users.noreply.github.com>
2023-07-21 17:27:37 +0000

    mobile screen idea added

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import './App.css';
 import { Routes, Route, } from "react-router-dom";
+import { useState, useEffect } from 'react';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import Section1 from './components/Sections/Section1';
@@ -12,41 +13,101 @@ import Bookstore from './components/Bookstore/Bookstore';
 import ScrollToTop from './ScrollToTop';
 
 function App() {
-  return(
+  return (
 
-  <ScrollToTop>
+    <ScrollToTop>
 
-  
-    <div className="App1">
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/publications" element={<PublicationPage />} /> 
-        <Route path="/bookstore" element={<Bookstore />} /> 
-      </Routes>
-    <div className="copyright">
-        <p>Copyright © 2022 MiracleAjah.com. All rights reserved.</p>
-    </div>
-    </div>
-  </ScrollToTop>
+
+      <div className="App1">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/publications" element={<PublicationPage />} />
+          <Route path="/bookstore" element={<Bookstore />} />
+        </Routes>
+        <div className="copyright">
+          <p>Copyright © 2022 MiracleAjah.com. All rights reserved.</p>
+        </div>
+      </div>
+    </ScrollToTop>
   )
 }
 
-function Home(){
-  return (
-    <div className="App">
+
+function Home() {
+  const [width, setWidth] = useState(window.innerWidth);
+  const isMobile = width <= 500;
+
+  const handleWindowSizeChange = () => {
+    setWidth(window.innerWidth);
+  };
+
+  useEffect(() => {
+    window.addEventListener('resize', handleWindowSizeChange);
+    return () => {
+      window.removeEventListener('resize', handleWindowSizeChange);
+    };
+  }, []);
+
+
+// FOR MOBILE VIEW
+  if (isMobile) {
+    return (
+      <div className="App">
       <div className="c1">
+        <Header />
+        <Section1 />
+      </div>
+      
+      <Section2 />
+      <Section3 />
+      <Section4 />
+      <Footer />
+
+
+    </div>
+    );
+  } 
+  
+  // FOR DESKTOP VIEW
+  else {
+    return (
+      <div className="App">
+        <div className="c1">
           <Header />
           <Section1 />
+        </div>
+        
+        <Section2 />
+        <Section3 />
+        <Section4 />
+        <Footer />
+
+
       </div>
 
-    <Section2 />
-    <Section3 />
-    <Section4 />
-    <Footer />
-
-    
-    </div>
-  );
+    );
+  }
 }
+
+
+
+// function Home() {
+//   return (
+//     <div className="App">
+//       <div className="c1">
+//         <Header />
+//         <Section1 />
+//       </div>
+//       <Mobile />
+//       <Section2 />
+//       <Section3 />
+//       <Section4 />
+//       <Footer />
+
+
+//     </div>
+//   );
+// }
+
 
 export default App;
